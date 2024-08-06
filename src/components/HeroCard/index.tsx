9c61bb174338df3f4ff729242b119ab8cce7409d
Ricardo <ricardopedroruiz@gmail.com>
2024-08-06 20:21:08 +0200

    added section title component

--- src/components/HeroCard/index.tsx
+++ src/components/HeroCard/index.tsx
@@ -1,27 +1,29 @@
+import Link from "next/link";
 import FavoriteStatusIcon from "../FavoriteStatusIcon";
 import HeroContainer from "../HeroContainer";
-import HeartIcon from "../Icons/HeartIcon";
 import { Hero } from "../types/heroes";
 import styles from "./HeroCard.module.css";
 import Image from "next/image";
 
 const HeroCard = ({ hero }: { hero: Hero }) => {
   return (
-    <HeroContainer>
-      <div className={styles.herocard}>
-        <Image
-          alt="hero image"
-          src="/img/marvel_logo.svg"
-          width={172.5}
-          height={189.97}
-        />
-        <div className={styles.separator} />
-        <div className={styles.bottomcard}>
-          <span className={styles.heroname}>ADAM WARLOK</span>
-          <FavoriteStatusIcon isFavorited={false} onClick={() => {}} />
+    <Link href="/hero">
+      <HeroContainer>
+        <div className={styles.herocard}>
+          <Image
+            alt="hero image"
+            src="/img/marvel_logo.svg"
+            width={172.5}
+            height={189.97}
+          />
+          <div className={styles.separator} />
+          <div className={styles.bottomcard}>
+            <span className={styles.heroname}>ADAM WARLOK</span>
+            <FavoriteStatusIcon isFavorited={false} onClick={() => {}} />
+          </div>
         </div>
-      </div>
-    </HeroContainer>
+      </HeroContainer>
+    </Link>
   );
 };
 
